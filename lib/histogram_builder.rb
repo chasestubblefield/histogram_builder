9fe38c087d123e4e2bda9ed6000d70a0bfd804a8
Chase Stubblefield <chasestubblefield@gmail.com>
2017-08-29 16:34:49 -0700

    Create executable

--- lib/histogram_builder.rb
+++ lib/histogram_builder.rb
@@ -1,4 +1,10 @@
+require 'json'
+
 require 'histogram_builder/version'
 
 module HistogramBuilder
+  def self.run(path)
+    data = JSON.parse(File.read(path), symbolize_names: true)
+    puts data
+  end
 end
